62f0a2959ec21cc20c7fcafeab8d37f46297a544
Tim Shaker <tim@tmshkr.com>
2020-02-28 22:11:38 -0800

    routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h2>Hello World</h2>
+      <Router>
+        <h1>DevDesk</h1>
+        {/* <SearchBar /> */}
+        <main className="main">
+          {/* <Tabs /> */}
+          <Switch>
+            <Route path="/dashboard">
+              <h2>Dashboard</h2>
+            </Route>
+            <Route path={["/search/:term/:page", "/search/:term", "/search"]}>
+              <h2>Search</h2>
+            </Route>
+            <Route exact path="/user">
+              <h2>UserProfile</h2>
+            </Route>
+            <Route path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}>
+              <h2>Ticket</h2>
+            </Route>
+            <Redirect to="/dashboard" />
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
