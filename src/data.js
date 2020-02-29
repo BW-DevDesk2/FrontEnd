3556029c8044f84c8b34b810ff53c65f4dffc08f
Tim Shaker <tim@tmshkr.com>
2020-02-28 22:11:26 -0800

    potential schema

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = {
+  user: {
+    id: "shortid",
+    name: "First Last",
+    createdAt: Date.now(),
+    role: {
+      isAdmin: Boolean,
+      isStudent: Boolean,
+      isHelper: Boolean
+    }
+  },
+  tickets: {
+    shortid: {
+      id: "shortid",
+      createdAt: Date.now(),
+      title: String,
+      description: String,
+      solutions: ["shortid"],
+      categories: [String],
+      isOpen: Boolean,
+      isResolved: Boolean,
+      assignedTo: ["shortid"],
+      meta: {
+        field: "value"
+      }
+    }
+  },
+  solutions: {
+    shortid: {
+      id: "shortid",
+      postedBy: "shortid",
+      createdAt: Date.now(),
+      text: String
+    }
+  }
+};
