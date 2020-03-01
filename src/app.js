464ea0bd5ab210aebbbb3a2b5db32f5548987307
Tim Shaker <tim@tmshkr.com>
2020-02-29 21:26:18 -0800

    use ol

--- src/app.js
+++ src/app.js
@@ -18,41 +18,41 @@ function App() {
           <Switch>
             <Route path="/dashboard">
               <h2>Dashboard</h2>
-              <ul className="ticket-feed">
+              <ol className="ticket-feed">
                 <li>Ticket 1</li>
                 <li>Ticket 2</li>
                 <li>Ticket 3</li>
-              </ul>
+              </ol>
             </Route>
             <Route path="/login">
               <h2>Login</h2>
             </Route>
             <Route path="/saved">
               <h2>Saved Tickets</h2>
-              <ul className="saved-tickets">
+              <ol className="saved-tickets">
                 <li>Ticket 1</li>
                 <li>Ticket 2</li>
                 <li>Ticket 3</li>
-              </ul>
+              </ol>
             </Route>
             <Route path="/signup">
               <h2>Signup</h2>
             </Route>
             <Route path={["/search/:term/:page", "/search/:term", "/search"]}>
               <h2>Search</h2>
-              <ul className="search-results">
+              <ol className="search-results">
                 <li>Ticket 1</li>
                 <li>Ticket 2</li>
                 <li>Ticket 3</li>
-              </ul>
+              </ol>
             </Route>
             <Route path="/user/:username">
               <h2>UserProfile</h2>
-              <ul className="user-tickets">
+              <ol className="user-tickets">
                 <li>Ticket 1</li>
                 <li>Ticket 2</li>
                 <li>Ticket 3</li>
-              </ul>
+              </ol>
             </Route>
             <Route path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}>
               <h2>Create / Edit / View Ticket</h2>
