1b36f2122b30b260067cff4ab9728ede74a9e297
Tim Shaker <tim@tmshkr.com>
2020-02-29 23:37:23 -0800

    singleton component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -8,61 +8,72 @@ import {
 
 import Header from "./components/header";
 
-function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Header />
-        <main>
-          {/* <Tabs /> */}
-          <Switch>
-            <Route path="/dashboard">
-              <h2>Dashboard</h2>
-              <ol className="ticket-feed">
-                <li>Ticket 1</li>
-                <li>Ticket 2</li>
-                <li>Ticket 3</li>
-              </ol>
-            </Route>
-            <Route path="/login">
-              <h2>Login</h2>
-            </Route>
-            <Route path="/saved">
-              <h2>Saved Tickets</h2>
-              <ol className="saved-tickets">
-                <li>Ticket 1</li>
-                <li>Ticket 2</li>
-                <li>Ticket 3</li>
-              </ol>
-            </Route>
-            <Route path="/signup">
-              <h2>Signup</h2>
-            </Route>
-            <Route path={["/search/:term/:page", "/search/:term", "/search"]}>
-              <h2>Search</h2>
-              <ol className="search-results">
-                <li>Ticket 1</li>
-                <li>Ticket 2</li>
-                <li>Ticket 3</li>
-              </ol>
-            </Route>
-            <Route path="/user/:username">
-              <h2>UserProfile</h2>
-              <ol className="user-tickets">
-                <li>Ticket 1</li>
-                <li>Ticket 2</li>
-                <li>Ticket 3</li>
-              </ol>
-            </Route>
-            <Route path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}>
-              <h2>Create / Edit / View Ticket</h2>
-            </Route>
-            <Redirect to="/dashboard" />
-          </Switch>
-        </main>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    app = this;
+    this.state = {
+      user: null
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <Header />
+          <main>
+            {/* <Tabs /> */}
+            <Switch>
+              <Route path="/dashboard">
+                <h2>Dashboard</h2>
+                <ol className="ticket-feed">
+                  <li>Ticket 1</li>
+                  <li>Ticket 2</li>
+                  <li>Ticket 3</li>
+                </ol>
+              </Route>
+              <Route path="/login">
+                <h2>Login</h2>
+              </Route>
+              <Route path="/saved">
+                <h2>Saved Tickets</h2>
+                <ol className="saved-tickets">
+                  <li>Ticket 1</li>
+                  <li>Ticket 2</li>
+                  <li>Ticket 3</li>
+                </ol>
+              </Route>
+              <Route path="/signup">
+                <h2>Signup</h2>
+              </Route>
+              <Route path={["/search/:term/:page", "/search/:term", "/search"]}>
+                <h2>Search</h2>
+                <ol className="search-results">
+                  <li>Ticket 1</li>
+                  <li>Ticket 2</li>
+                  <li>Ticket 3</li>
+                </ol>
+              </Route>
+              <Route path="/user/:username">
+                <h2>UserProfile</h2>
+                <ol className="user-tickets">
+                  <li>Ticket 1</li>
+                  <li>Ticket 2</li>
+                  <li>Ticket 3</li>
+                </ol>
+              </Route>
+              <Route path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}>
+                <h2>Create / Edit / View Ticket</h2>
+              </Route>
+              <Redirect to="/dashboard" />
+            </Switch>
+          </main>
+        </Router>
+      </div>
+    );
+  }
 }
 
+export let app;
 export default App;
