0b6d32e431ef69312b46bd6f5f251c837314382d
Tim Shaker <tim@tmshkr.com>
2020-03-01 19:08:34 -0800

    authenticated/unauthenticated routes

--- src/app.js
+++ src/app.js
@@ -25,37 +25,46 @@ class App extends Component {
         <Router>
           <Header />
           <main>
-            <Tabs />
             <Switch>
-              <Route path="/dashboard">
-                <Dashboard />
-              </Route>
+              {/* unauthenticated */}
               <Route path="/login">
                 <h2>Login</h2>
               </Route>
               <Route path="/signup">
                 <h2>Signup</h2>
               </Route>
-              <Route path={["/search/:term/:page", "/search/:term", "/search"]}>
-                <h2>Search</h2>
-                <ol className="search-results">
-                  <li>Ticket 1</li>
-                  <li>Ticket 2</li>
-                  <li>Ticket 3</li>
-                </ol>
+              {/* authenticated */}
+              <Route>
+                <Tabs />
+                <Route path="/dashboard">
+                  <Dashboard />
+                </Route>
+                <Route
+                  path={["/search/:term/:page", "/search/:term", "/search"]}
+                >
+                  <h2>Search</h2>
+                  <ol className="search-results">
+                    <li>Ticket 1</li>
+                    <li>Ticket 2</li>
+                    <li>Ticket 3</li>
+                  </ol>
+                </Route>
+                <Route path="/user/:id">
+                  <h2>UserProfile</h2>
+                  <ol className="ticket-list">
+                    <li>Ticket 1</li>
+                    <li>Ticket 2</li>
+                    <li>Ticket 3</li>
+                  </ol>
+                </Route>
+                <Route
+                  path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}
+                >
+                  <h2>Create / Edit / View Ticket</h2>
+                </Route>
+                <Redirect to="/dashboard" />
               </Route>
-              <Route path="/user/:id">
-                <h2>UserProfile</h2>
-                <ol className="ticket-list">
-                  <li>Ticket 1</li>
-                  <li>Ticket 2</li>
-                  <li>Ticket 3</li>
-                </ol>
-              </Route>
-              <Route path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}>
-                <h2>Create / Edit / View Ticket</h2>
-              </Route>
-              <Redirect to="/tickets" />
+              <Redirect to="/signup" />
             </Switch>
           </main>
         </Router>
