a42322e248cecd0af243640d201434c164a37335
Tim Shaker <tim@tmshkr.com>
2020-03-02 11:30:52 -0800

    include reactstrap components

--- src/components/auth/login-form.js
+++ src/components/auth/login-form.js
@@ -1,36 +1,60 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Button, FormGroup, Label } from "reactstrap";
+import { useHistory } from "react-router-dom";
 
 function LoginForm(props) {
+  const history = useHistory();
   const { handleSubmit, register, errors } = useForm();
   const onSubmit = values => {
     console.log(values);
   };
 
   return (
-    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        name="email"
-        ref={register({
-          required: "Required",
-          pattern: {
-            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-            message: "invalid email address"
-          }
-        })}
-      />
-      {errors.email && errors.email.message}
+    <form className="form login-form" onSubmit={handleSubmit(onSubmit)}>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <input
+          className="form-control"
+          name="email"
+          type="email"
+          id="email"
+          ref={register({
+            required: "Required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "invalid email address"
+            }
+          })}
+        />
+        {errors.email && errors.email.message}
+      </FormGroup>
+      <FormGroup>
+        <Label for="password">Password</Label>
+        <input
+          className="form-control"
+          type="password"
+          name="password"
+          id="password"
+          ref={register({
+            required: "Required",
+            validate: value => value !== "password" || "Nice try!"
+          })}
+        />
+        {errors.password && errors.password.message}
+      </FormGroup>
 
-      <input
-        name="password"
-        type="password"
-        ref={register({
-          validate: value => value !== "password" || "Nice try!"
-        })}
-      />
-      {errors.password && errors.password.message}
-
-      <button type="submit">Submit</button>
+      <Button type="submit" color="primary" size="lg" block>
+        Login
+      </Button>
+      <Button
+        color="info"
+        size="lg"
+        block
+        onClick={() => history.push("/signup")}
+      >
+        Signup
+      </Button>
     </form>
   );
 }
