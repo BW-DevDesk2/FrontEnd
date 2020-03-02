f200893b1369d978b4fbbcb824e9acaf12a3b330
Tim Shaker <tim@tmshkr.com>
2020-03-01 19:16:18 -0800

    create router.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
-} from "react-router-dom";
-
-import Header from "./components/header";
-import Tabs from "./components/tabs";
-import Dashboard from "./components/dashboard";
+import Router from "./components/router";
 
 class App extends Component {
   constructor() {
@@ -20,50 +11,7 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <Router>
-        <Header />
-        <main>
-          <Switch>
-            {/* unauthenticated */}
-            <Route path="/login">
-              <h2>Login</h2>
-            </Route>
-            <Route path="/signup">
-              <h2>Signup</h2>
-            </Route>
-            {/* authenticated */}
-            <Route>
-              <Tabs />
-              <Route path="/dashboard">
-                <Dashboard />
-              </Route>
-              <Route path={["/search/:term/:page", "/search/:term", "/search"]}>
-                <h2>Search</h2>
-                <ol className="search-results">
-                  <li>Ticket 1</li>
-                  <li>Ticket 2</li>
-                  <li>Ticket 3</li>
-                </ol>
-              </Route>
-              <Route path="/user/:id">
-                <h2>UserProfile</h2>
-                <ol className="ticket-list">
-                  <li>Ticket 1</li>
-                  <li>Ticket 2</li>
-                  <li>Ticket 3</li>
-                </ol>
-              </Route>
-              <Route path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}>
-                <h2>Create / Edit / View Ticket</h2>
-              </Route>
-              <Redirect to="/dashboard" />
-            </Route>
-            <Redirect to="/signup" />
-          </Switch>
-        </main>
-      </Router>
-    );
+    return <Router />;
   }
 }
 
---
+++ src/components/router.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import Header from "./header";
+import Tabs from "./tabs";
+import Dashboard from "./dashboard";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Switch>
+          {/* unauthenticated */}
+          <Route path="/login">
+            <h2>Login</h2>
+          </Route>
+          <Route path="/signup">
+            <h2>Signup</h2>
+          </Route>
+          {/* authenticated */}
+          <Route>
+            <Tabs />
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path={["/search/:term/:page", "/search/:term", "/search"]}>
+              <h2>Search</h2>
+              <ol className="search-results">
+                <li>Ticket 1</li>
+                <li>Ticket 2</li>
+                <li>Ticket 3</li>
+              </ol>
+            </Route>
+            <Route path="/user/:id">
+              <h2>UserProfile</h2>
+              <ol className="ticket-list">
+                <li>Ticket 1</li>
+                <li>Ticket 2</li>
+                <li>Ticket 3</li>
+              </ol>
+            </Route>
+            <Route path={["/ticket/new", "/ticket/:id/edit", "/ticket/:id"]}>
+              <h2>Create / Edit / View Ticket</h2>
+            </Route>
+            <Redirect to="/dashboard" />
+          </Route>
+          <Redirect to="/signup" />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
