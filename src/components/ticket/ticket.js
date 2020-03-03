bdd5eef6e15e9a7aea1f9de011a031157cc2ba24
Tim Shaker <tim@tmshkr.com>
2020-03-03 11:36:17 -0800

    toggleClaimed toggleResolved

--- src/components/ticket/ticket.js
+++ src/components/ticket/ticket.js
@@ -17,19 +17,40 @@ function Ticket() {
   const [ticket, setTicket] = useState();
 
   useEffect(() => {
-    axios.get(`/api/tickets/${id}`).then(({ data }) => setTicket(data));
+    axios.get(`/api/tickets/${id}`).then(({ data }) => {
+      console.log(data);
+      setTicket(data);
+    });
   }, []);
+
+  const toggleClaimed = () => {
+    const update = { ...ticket };
+    update.statusesid === 3 ? (update.statusesid = 1) : (update.statusesid = 3);
+    axios.put(`/api/tickets/${id}`, update).then(() => setTicket(update));
+  };
+
+  const toggleResolved = () => {
+    const update = { ...ticket };
+    update.statusesid === 2 ? (update.statusesid = 3) : (update.statusesid = 2);
+    axios.put(`/api/tickets/${id}`, update).then(() => setTicket(update));
+  };
+
+  // const deleteTicket = () => {};
 
   if (!ticket) return <Spinner color="primary" />;
 
-  const { title, description } = ticket;
+  const { title, description, statusesid } = ticket;
   return (
     <Card>
       <CardBody>
         <CardTitle>{title}</CardTitle>
         <CardText>{description}</CardText>
-        <Button color="primary">Claim</Button>
-        <Button color="success">Resolved</Button>
+        <Button color="primary" onClick={toggleClaimed}>
+          {statusesid === 3 ? "Claimed" : "Claim"}
+        </Button>
+        <Button color="success" onClick={toggleResolved}>
+          {statusesid === 2 ? "Resolved" : "Resolve"}
+        </Button>
         <Button color="danger">Delete</Button>
       </CardBody>
     </Card>
