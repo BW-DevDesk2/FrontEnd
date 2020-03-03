9a67436acfcf926623dd4b518235815f7e85c72f
Tim Shaker <tim@tmshkr.com>
2020-03-03 10:18:38 -0800

    ticket card

--- src/components/ticket/ticket.js
+++ src/components/ticket/ticket.js
@@ -1,15 +1,29 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Card, CardText, CardBody, CardTitle, Button } from "reactstrap";
 
 import { AuthContext } from "../../app";
 
 function Ticket() {
   const { axios } = useContext(AuthContext)();
+  const { id } = useParams();
+  const [ticket, setTicket] = useState({});
 
   useEffect(() => {
-    axios.get("/api/tickets").then(response => console.log(response));
+    axios.get(`/api/tickets/${id}`).then(({ data }) => setTicket(data));
   }, []);
 
-  return <h2>ticket.js</h2>;
+  const { title, description } = ticket;
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+        <Button color="primary">Claim</Button>
+        <Button color="danger">Resolved</Button>
+      </CardBody>
+    </Card>
+  );
 }
 
 export default Ticket;
