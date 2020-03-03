3b0c1b30b87bcdbbfd194d546c4aa65a005719fb
jgreenberg3 <jgreenberg@runbox.com>
2020-03-02 19:02:19 -0500

    updated

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+function UserProfile(props) {
+  const [user, setUser] = useState("");
 
-function UserProfile (){
+  useEffect(() => {
+    const getUser = () => {
+      axios
+        .get("https://devdesk2eli.herokuapp.com/api/users/:id")
+        .then(response => {
+          console.log(response);
+          setUser(response);
+        })
+        .catch(error => {
+          console.log("Error: ", error);
+        });
+    };
+    getUser();
+  }, user);
 
-
-
-
-    return();
+  return (
+    <div>
+      <div></div>
+      <h1>My Profile</h1>
+      <h2>{props.name}</h2>
+      <h3>Email: </h3>
+      <p>Date Joined: </p>
+      <p>Settings: </p>
+    </div>
+  );
 }
 
 export default UserProfile;
