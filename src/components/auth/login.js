6d947d3c63622ed7d819e7c24e854d7832d7edd2
Tim Shaker <tim@tmshkr.com>
2020-03-04 22:33:19 -0800

    loading spinner for login/register

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Button, FormGroup, Label } from "reactstrap";
+import { Button, FormGroup, Label, Spinner } from "reactstrap";
 
 import { AuthContext } from "../../app";
 
 function LoginForm(props) {
   const { axios, login } = useContext(AuthContext)();
+  const [loading, setLoading] = useState(false);
   const { history } = props;
   const { handleSubmit, register, errors, setError } = useForm();
 
   const onSubmit = values => {
+    setLoading(true);
     axios
       .post("/api/login", values)
       .then(response => {
@@ -35,53 +37,60 @@ function LoginForm(props) {
   };
 
   return (
-    <form className="form auth-form" onSubmit={handleSubmit(onSubmit)}>
-      <FormGroup>
-        <Label for="email">Email</Label>
-        <input
-          className="form-control"
-          name="email"
-          type="email"
-          id="email"
-          ref={register({
-            required: "Required",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-              message: "invalid email address"
-            }
-          })}
-        />
-        <span className="error">{errors.email && errors.email.message}</span>
-      </FormGroup>
-      <FormGroup>
-        <Label for="password">Password</Label>
-        <input
-          className="form-control"
-          type="password"
-          name="password"
-          id="password"
-          ref={register({
-            required: "Required",
-            validate: value => value !== "password" || "Use a better password"
-          })}
-        />
-        <span className="error">
-          {errors.password && errors.password.message}
-        </span>
-      </FormGroup>
+    <>
+      <form className="form auth-form" onSubmit={handleSubmit(onSubmit)}>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <input
+            className="form-control"
+            name="email"
+            type="email"
+            id="email"
+            ref={register({
+              required: "Required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "invalid email address"
+              }
+            })}
+          />
+          <span className="error">{errors.email && errors.email.message}</span>
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            id="password"
+            ref={register({
+              required: "Required",
+              validate: value => value !== "password" || "Use a better password"
+            })}
+          />
+          <span className="error">
+            {errors.password && errors.password.message}
+          </span>
+        </FormGroup>
 
-      <Button type="submit" color="primary" size="lg" block>
-        Login
-      </Button>
-      <Button
-        color="info"
-        size="lg"
-        block
-        onClick={() => history.push("/signup")}
-      >
-        Sign Up
-      </Button>
-    </form>
+        {!loading && (
+          <>
+            <Button type="submit" color="primary" size="lg" block>
+              Login
+            </Button>
+            <Button
+              color="info"
+              size="lg"
+              block
+              onClick={() => history.push("/signup")}
+            >
+              Sign Up
+            </Button>
+          </>
+        )}
+      </form>
+      {loading && <Spinner color="primary" />}
+    </>
   );
 }
 
--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -1,12 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
-import { Button, FormGroup, Label } from "reactstrap";
+import { Button, FormGroup, Label, Spinner } from "reactstrap";
 
 import { AuthContext } from "../../app";
 
 function SignUpForm(props) {
   const { axios, login } = useContext(AuthContext)();
+  const [loading, setLoading] = useState(false);
   const { history } = props;
 
   const schema = yup.object().shape({
@@ -28,7 +29,8 @@ function SignUpForm(props) {
   });
 
   const onSubmit = values => {
-    delete values.role; // delete role until backend can handle it
+    setLoading(true);
+    delete values.role;
     axios.post("/api/register", values).then(response => {
       console.log(response);
       const user = response.data;
@@ -38,64 +40,69 @@ function SignUpForm(props) {
   };
 
   return (
-    <form className="form auth-form" onSubmit={handleSubmit(onSubmit)}>
-      <FormGroup>
-        <Label for="name">Name</Label>
-        <input
-          className="form-control"
-          name="name"
-          type="name"
-          id="name"
-          placeholder="Your Name"
-          ref={register()}
-        />
-        <span className="error">{errors.name && errors.name.message}</span>
-      </FormGroup>
-      <FormGroup>
-        <Label for="email">Email</Label>
-        <input
-          className="form-control"
-          name="email"
-          type="email"
-          id="email"
-          placeholder="you@example.com"
-          ref={register()}
-        />
-        <span className="error">{errors.email && errors.email.message}</span>
-      </FormGroup>
-      <FormGroup>
-        <Label for="password">Password</Label>
-        <input
-          className="form-control"
-          type="password"
-          name="password"
-          id="password"
-          ref={register()}
-        />
-        <span className="error">
-          {errors.password && errors.password.message}
-        </span>
-      </FormGroup>
-      <FormGroup>
-        <Label for="select">Role</Label>
-        <select
-          id="select"
-          className="form-control"
-          type="select"
-          name="role"
-          ref={register()}
-        >
-          <option disabled>Choose one</option>
-          <option>Student</option>
-          <option>Helper</option>
-          <option>Admin</option>
-        </select>
-        <span className="error">{errors.role && errors.role.message}</span>
-      </FormGroup>
-      <Button type="submit" color="primary" size="lg" block>
-        Sign Up
-      </Button>
-    </form>
+    <>
+      <form className="form auth-form" onSubmit={handleSubmit(onSubmit)}>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <input
+            className="form-control"
+            name="name"
+            type="name"
+            id="name"
+            placeholder="Your Name"
+            ref={register()}
+          />
+          <span className="error">{errors.name && errors.name.message}</span>
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <input
+            className="form-control"
+            name="email"
+            type="email"
+            id="email"
+            placeholder="you@example.com"
+            ref={register()}
+          />
+          <span className="error">{errors.email && errors.email.message}</span>
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            id="password"
+            ref={register()}
+          />
+          <span className="error">
+            {errors.password && errors.password.message}
+          </span>
+        </FormGroup>
+        <FormGroup>
+          <Label for="select">Role</Label>
+          <select
+            id="select"
+            className="form-control"
+            type="select"
+            name="role"
+            ref={register()}
+          >
+            <option disabled>Choose one</option>
+            <option>Student</option>
+            <option>Helper</option>
+            <option>Admin</option>
+          </select>
+          <span className="error">{errors.role && errors.role.message}</span>
+        </FormGroup>
+        {!loading && (
+          <Button type="submit" color="primary" size="lg" block>
+            Sign Up
+          </Button>
+        )}
+      </form>
+      {loading && <Spinner color="primary" />}
+    </>
   );
 }
 
