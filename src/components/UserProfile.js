c9fd649f81df1929ed832447df2c96af90a7b595
jgreenberg3 <jgreenberg@runbox.com>
2020-03-05 14:49:11 -0500

    update

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../app";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button,
+  Spinner
+} from "reactstrap";
 
 function UserProfile(props) {
   const [user, setUser] = useState([]);
@@ -12,7 +20,7 @@ function UserProfile(props) {
   const username = JSON.parse(window.localStorage.getItem("user"));
   const userID = username.usersid;
 
-  //
+  // RETRIEVE PROFILE DATA
 
   useEffect(() => {
     const getUser = () => {
@@ -44,17 +52,67 @@ function UserProfile(props) {
     getRoles();
   }, []);
 
+  // FORM TO MAKE CHANGES TO PROFILE IN DATABASE
+
+  // function Ticket() {
+  //   const { axios, user } = useContext(AuthContext)();
+  //   const { id } = useParams();
+  //   const history = useHistory();
+  //   const [ticket, setTicket] = useState();
+
+  //   useEffect(() => {
+  //     axios.get(`/api/tickets/${id}`).then(({ data }) => {
+  //       console.log(data);
+  //       setTicket(data);
+  //     });
+  //   }, []);
+
+  //   const toggleClaimed = () => {
+  //     const update = { ...ticket };
+  //     update.statusesid === 3
+  //       ? (update.statusesid = 1)
+  //       : (update.statusesid = 3);
+  //     axios.put(`/api/tickets/${id}`, update).then(() => setTicket(update));
+  //   };
+
+  //   const toggleResolved = () => {
+  //     const update = { ...ticket };
+  //     update.statusesid === 2
+  //       ? (update.statusesid = 3)
+  //       : (update.statusesid = 2);
+  //     axios.put(`/api/tickets/${id}`, update).then(() => setTicket(update));
+  //   };
+
+  //   const deleteTicket = () => {
+  //     axios.delete(`/api/tickets/${id}`).then(() => history.push("/dashboard"));
+  //   };
+
+  //   if (!ticket) return <Spinner color="primary" />;
+
+  //   const { title, description, statusesid } = ticket;
+  // }
+
+  //
+
   return (
-    <div>
-      <h1>My Profile</h1>
-      <b>Name: </b>
-      {user.name}
-      <br></br>
-      <b>Role: </b>
-      {roles}
-      <br></br>
-      <b>Email: </b> {user.email}
-    </div>
+    <Card>
+      <CardBody>
+        <CardTitle>My Profile </CardTitle>
+        <CardTitle>
+          <b>Name:</b> {user.name}
+        </CardTitle>
+        <CardTitle>
+          <b>Email:</b> {user.role}
+        </CardTitle>
+        <CardTitle>
+          <b>Role:</b> {user.email}
+        </CardTitle>
+        <CardText></CardText>
+        <Button color="primary">button</Button>
+        <Button color="success">button</Button>
+        <Button color="danger">Delete</Button>
+      </CardBody>
+    </Card>
   );
 }
 
